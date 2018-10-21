d3ec6e8aa5aef48bd417f67cce849e39ddfbc3d8
Ben Herring <ben_herring@hotmail.com>
2018-10-21 15:56:35 +0100

    progress with following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,19 @@ class UsersController < ApplicationController
   end
 
   def follow
-     @user = User.find(params[:id])
-      current_user.follow(@user)
-      redirect to user_path(@user)
-    # @current_user.follow(@user)
-    # @follow = Follow.find_by(follower: @current_user, followable: @user)
+     # @user = User.find(params[:id])
+     #  current_user.follow(@user)
+     #  redirect to user_path(@user)
+    @current_user.follow(@user)
+    @follow = Follow.find_by(follower: @current_user, followable: @user)
     # respond_to :js
   end
 
   def unfollow
-    @user = User.find(params[:id])
-    current_user.stop_following(@user)
-    redirect_to user_path(@user)
-    # @current_user.stop_following(@user)
+    # @user = User.find(params[:id])
+    # current_user.stop_following(@user)
+    # redirect_to user_path(@user)
+    @current_user.stop_following(@user)
     # respond_to :js
   end
 end
